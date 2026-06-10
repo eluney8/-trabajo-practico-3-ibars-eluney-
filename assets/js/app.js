5d498f656075479b33ba369e74fd5361f077b968
eluney8 <eluneyibarstsdsm@gmail.com>
2026-06-10 20:44:45 -0300

    feat: se realizó el renderizado de las cards.

--- assets/js/app.js
+++ assets/js/app.js
@@ -20,3 +20,36 @@ const obtenerPersonajes = async () => {
     console.error("Error:", error);
   }
 };
+
+const renderizarCards = (lista) => {
+  cardContainer.innerHTML = "";
+
+  lista.forEach((personaje) => {
+    const { id, name, occupation, status, portrait_path } = personaje;
+
+    const urlImagenCompleta = `${BASE_CDN}${portrait_path}`;
+
+    cardContainer.innerHTML += `
+            <div class="col-12 col-md-6 col-lg-4 mb-4">
+                <div class="card h-100 shadow-sm border-warning">
+                    <img 
+                        src="${urlImagenCompleta}" 
+                        class="card-img-top p-2" 
+                        alt="${name}" 
+                        style="height: 250px; object-fit: contain;"
+                    >
+                    <div class="card-body d-flex flex-column text-center">
+                        <h5 class="card-title text-primary fw-bold">${name}</h5>
+                        <p class="card-text mb-1 small"><strong>Ocupación:</strong> ${occupation}</p>
+                        <p class="card-text text-muted small"><strong>Estado:</strong> ${status}</p>
+                        
+                        <
+                        <button class="btn btn-warning mt-auto fw-bold btn-ver-detalle" data-id="${id}">
+                            Ver detalle
+                        </button>
+                    </div>
+                </div>
+            </div>
+        `;
+  });
+};
